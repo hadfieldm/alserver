ff7a6c6f3450775507082ea620b1f01a0d09ae07
hadfielm <hadfielm@gmail.com>
2017-03-22 21:22:18 +1300

    removed passport features from db server

--- src/config.js
+++ src/config.js
@@ -1,8 +1,3 @@
 export default {
-  jwtSecret: 'jwtTokenString',
-  'googleAuth': {
-      'clientID': '384898356338-59g0l30nsoel78k3qa52ptleomqheuc7.apps.googleusercontent.com',
-      'clientSecret': '-ck1P1mxvMqFMXb2AwVNCrLa',
-      'callbackURL': 'http://localhost:8080/gauth/google/callback'
-  }
+  jwtSecret: 'jwtTokenString'
 }
